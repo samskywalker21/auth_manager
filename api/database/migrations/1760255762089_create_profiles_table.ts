bc6c2512ecca8bf74f1688c664f865b76225eca1
Sam Daniel <sammedino@gmail.com>
2025-10-13 09:49:30 +0800

    Update 1760255762089_create_profiles_table.ts

--- api/database/migrations/1760255762089_create_profiles_table.ts
+++ api/database/migrations/1760255762089_create_profiles_table.ts
@@ -1,24 +1,24 @@
-import { BaseSchema } from '@adonisjs/lucid/schema'
+import { BaseSchema } from '@adonisjs/lucid/schema';
 
 export default class extends BaseSchema {
-  protected tableName = 'profile'
+  protected tableName = 'profile';
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id').primary().unsigned()
-      table.string('first_name', 30).notNullable()
-      table.string('middle_name', 30)
-      table.string('last_name', 30).notNullable()
-      table.string('username', 30).unique().notNullable()
-      table.string('password').notNullable()
-      table.enum('status', ['A', 'I']).notNullable().defaultTo('A')
-      table.integer('section_id').unsigned().references('id').inTable('section')
-      table.timestamp('created_at')
-      table.timestamp('updated_at')
-    })
+      table.increments('id').primary().unsigned();
+      table.string('first_name', 30).notNullable();
+      table.string('middle_name', 30);
+      table.string('last_name', 30).notNullable();
+      table.string('username', 30).unique().notNullable();
+      table.string('password').notNullable();
+      table.enum('status', ['A', 'I']).notNullable().defaultTo('A');
+      table.integer('section_id').unsigned().references('id').inTable('section');
+      table.timestamp('created_at');
+      table.timestamp('updated_at');
+    });
   }
 
   async down() {
-    this.schema.dropTable(this.tableName)
+    this.schema.dropTable(this.tableName);
   }
 }
